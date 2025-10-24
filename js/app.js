a84101287fce6906114b558e4ad28d2697714e03
jaycartnine4 <ihatestaubin94@gmail.com>
2025-10-23 21:32:54 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,12 +1,24 @@
+let gridX = 0
+let gridY = 0
+let gridScale = 50
+
+const sizeX = 500;
+const sizeY = 600;
+
 function draw() {
   const canvas = document.getElementById("grid");
   const ctx = canvas.getContext("2d");
 
-  ctx.fillStyle = "rgb(200 0 0)";
-  ctx.fillRect(10, 10, 50, 50);
+  // horizontal
+  for(let i=0; i < sizeY / gridScale; i++) {
+    ctx.fillStyle = "rgb(45 45 45)";
+    ctx.fillRect(0, gridY+i*gridScale, sizeX, 1);
+  }
 
-  ctx.fillStyle = "rgb(0 0 200 / 50%)";
-  ctx.fillRect(30, 30, 50, 50);
+  // vertical
+  for(let i=0; i < sizeX / gridScale; i++) {
+    ctx.fillStyle = "rgb(0145 0145 45 )";
+    ctx.fillRect(gridX+i*gridScale, 0, 1, sizeY);
+  }
 }
-
 draw();
