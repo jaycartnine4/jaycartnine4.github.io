0a2982b9be0c42e08385060652d75eaf31f66a30
jaycartnine4 <ihatestaubin94@gmail.com>
2025-10-23 20:48:28 -0600

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,12 @@
+function draw() {
+  const canvas = document.getElementById("grid");
+  const ctx = canvas.getContext("2d");
+
+  ctx.fillStyle = "rgb(200 0 0)";
+  ctx.fillRect(10, 10, 50, 50);
+
+  ctx.fillStyle = "rgb(0 0 200 / 50%)";
+  ctx.fillRect(30, 30, 50, 50);
+}
+
+draw();
