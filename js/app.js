18d53776851698a43c863a5f83f7d2777b15f663
Jonathan Duck <duckbrain30@gmail.com>
2025-10-23 21:44:55 -0600

    Add width/height

--- js/app.js
+++ js/app.js
@@ -1,24 +1,26 @@
-let gridX = 0
-let gridY = 0
-let gridScale = 50
+let gridX = 0;
+let gridY = 0;
+let gridScale = 50;
 
 const sizeX = 500;
 const sizeY = 600;
 
 function draw() {
   const canvas = document.getElementById("grid");
+  canvas.clientWidth = document.body.clientWidth;
+  canvas.clientHeight = document.body.clientHeight;
   const ctx = canvas.getContext("2d");
 
   // horizontal
-  for(let i=0; i < sizeY / gridScale; i++) {
+  for (let i = 0; i < sizeY / gridScale; i++) {
     ctx.fillStyle = "rgb(45 45 45)";
-    ctx.fillRect(0, gridY+i*gridScale, sizeX, 1);
+    ctx.fillRect(0, gridY + i * gridScale, sizeX, 1);
   }
 
   // vertical
-  for(let i=0; i < sizeX / gridScale; i++) {
+  for (let i = 0; i < sizeX / gridScale; i++) {
     ctx.fillStyle = "rgb(0145 0145 45 )";
-    ctx.fillRect(gridX+i*gridScale, 0, 1, sizeY);
+    ctx.fillRect(gridX + i * gridScale, 0, 1, sizeY);
   }
 }
 draw();
