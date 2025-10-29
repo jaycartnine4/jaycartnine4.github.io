4a3e4c4fc1969d84e8b9ab598100dda98686e1c6
jared <ihatestaubin94@gmail.com>
2025-10-28 22:35:41 -0600

    diagonals, kinda Y too

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ let gridScale = 50;
 
 const sizeX = 500;
 const sizeY = 600;
+const isoSlope = 1.733;
 
 function draw() {
   const canvas = document.getElementById("grid");
@@ -11,16 +12,38 @@ function draw() {
   canvas.clientHeight = document.body.clientHeight;
   const ctx = canvas.getContext("2d");
 
-  // horizontal
-  for (let i = 0; i < sizeY / gridScale; i++) {
-    ctx.fillStyle = "rgb(45 45 45)";
-    ctx.fillRect(0, gridY + i * gridScale, sizeX, 1);
+  // X
+  for (let i = 0; i < (sizeY + sizeX / isoSlope) / gridScale; i++) {
+    const h = i * gridScale;
+    const w = h * isoSlope;
+    ctx.beginPath();
+    ctx.moveTo(0, sizeY - h);
+    ctx.lineTo(w, sizeY);
+    ctx.closePath();
+    ctx.stroke();
   }
 
-  // vertical
+  // Y
   for (let i = 0; i < sizeX / gridScale; i++) {
-    ctx.fillStyle = "rgb(0145 0145 45 )";
-    ctx.fillRect(gridX + i * gridScale, 0, 1, sizeY);
+    const h = i * gridScale;
+    const w = h * isoSlope;
+    ctx.beginPath();
+    ctx.moveTo(gridX + w / 2, 0);
+    ctx.lineTo(gridX + w / 2, sizeY);
+    ctx.closePath();
+    ctx.stroke();
+  }
+
+  // Z
+  for (let i = 0; i < (sizeY + sizeX / isoSlope) / gridScale; i++) {
+    const h = i * gridScale;
+    const w = h * isoSlope;
+    ctx.beginPath();
+    ctx.moveTo(sizeX, sizeY - h);
+    ctx.lineTo(sizeX - w, sizeY);
+    ctx.closePath();
+    ctx.stroke();
   }
 }
+
 draw();
